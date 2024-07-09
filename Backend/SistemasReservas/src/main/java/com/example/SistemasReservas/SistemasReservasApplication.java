ecca4bc878833f14c33c642f8c9a37bebd591f19
UP210537 <113135605+UP210537@users.noreply.github.com>
2024-07-09 17:55:27 -0600

    Add files via upload

---
+++ Backend/SistemasReservas/src/main/java/com/example/SistemasReservas/SistemasReservasApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemasReservas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemasReservasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemasReservasApplication.class, args);
+	}
+
+}
