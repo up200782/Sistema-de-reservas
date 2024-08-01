6d80bf7cde1f99d98facfc80d269282a7c9676b4
SofyO <up210419@alumnos.upa.edu.mx>
2024-07-31 21:06:05 -0600

    Actualizando

--- Frontent/src/componentes/ReservationForm/ReservationForm.js
+++ Frontent/src/componentes/ReservationForm/ReservationForm.js
@@ -4,7 +4,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-export default function SelectVariants() {
+export default function ReservationForm() {
   const [age, setAge] = React.useState('');
 
   const handleChange = (event) => {
